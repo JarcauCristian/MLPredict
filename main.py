0827db3ffbfc29d0757f9221dc4fb77254aff93e
JarcauCristian <stefan.jarcau@student.unitbv.ro>
2022-09-15 11:01:26 +0300

    Making a graph for the UI

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+
 from flask import Flask, request, render_template
-from methodes import get_one_prediction, get_all_prediction
+from methodes import get_one_prediction, get_all_prediction, get_one_prediction_all_data
 
 app = Flask(__name__)
 
@@ -20,6 +22,9 @@ def get_predictions():
             data = get_one_prediction(model_version, int(pat_id))
         elif all_pts == "all":
             data = get_all_prediction(model_version)
+        elif all_pts == "graph":
+            data = get_one_prediction_all_data(model_version, int(pat_id))
+            return render_template("graphs.html", data=data, list_of_items=data[int(pat_id)][1], letters=data[int(pat_id)][0])
         return render_template("prediction.html", data=data)
 
 
@@ -28,7 +33,16 @@ def my_utility_processor():
     def round_number(number):
         return round(number, 5)
 
-    return dict(round=round_number)
+    def array_len(array):
+        return len(array)
+
+    def round_to_int(number):
+        return int(round(number, 2) * 100)
+
+    def return_pat_id(pat_id):
+        return "Patient id:" + str(pat_id)
+
+    return dict(round=round_number, len=array_len, id=return_pat_id, rnd=round_to_int)
 
 
 if __name__ == '__main__':
--- methodes.py
+++ methodes.py
@@ -16,13 +16,26 @@ def get_one_prediction(model_version, pat_id):
     return data
 
 
+def get_one_prediction_all_data(model_version, pat_id):
+    json_request = {'model_version': 1,
+                    'patId': 1}
+    headers = {'Content-Type': 'application/json'}
+    json_request['model_version'] = model_version
+    json_request['patId'] = pat_id
+    data = {}
+    rsp = requests.post('http://147.102.33.191:29000/heartf/predict', data=json.dumps(json_request), headers=headers)
+    if rsp.status_code == 200 or rsp.status_code == 201:
+        data[pat_id] = [regulate_data(rsp.json()['prediction']), rsp.json()['day']]
+    return data
+
+
 def get_all_prediction(model_version):
     json_request = {'model_version': 1,
                     'patId': 1}
     headers = {'Content-Type': 'application/json'}
     json_request['model_version'] = model_version
     data = {}
-    for i in range(0, 300):
+    for i in range(100271, 100570):
         json_request['patId'] = i
         rsp = requests.post('http://147.102.33.191:29000/heartf/predict', data=json.dumps(json_request), headers=headers)
         if rsp.status_code == 200 or rsp.status_code == 201:
@@ -40,3 +53,10 @@ def get_maximum_from_array(array):
             pos = i
 
     return pos
+
+
+def regulate_data(array):
+    data = []
+    for i in array:
+        data.append(int(round(i, 2) * 100))
+    return data
